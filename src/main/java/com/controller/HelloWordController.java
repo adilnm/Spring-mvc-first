c9e80603d17e4c38ec13034df69974df6fc69eb6
Adil Naimi <adilnm23@gmail.com>
2020-12-31 22:50:28 -0600

    Crete mvc

---
+++ src/main/java/com/controller/HelloWordController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWordController {
+
+	@GetMapping("/hellooo")
+	public String showHello() {
+		return "hello";
+	}
+
+	@GetMapping("/show")
+	public String showDetail() {
+		return "showDetails";
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsps/showDetails.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>My Details</h1>
+	<ul>
+		<li>Name: Kevin</li>
+		<li>Address: Westheimer Rd .....</li>
+		<li>Phone: 2222222222</li>
+	</ul>
+</body>
+</html>
